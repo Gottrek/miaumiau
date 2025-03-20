ba1ad8927abbde52fed76fabe7911a2a02c908d1
Gotrek <wiktorsinkiewicz@gmail.com>
2025-03-20 18:41:00 +0100

    bubble

---
+++ bubble_sort.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "sorts.h"
+
+using namespace std;
+
+void bubble_sort(vector<int>& arr)
+{
+    int n = arr.size();
+    int temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                liczba_zamian++;
+            }
+            liczba_porownan++;
+        }
+    }
+}
--- sorts.h
+++ sorts.h
@@ -11,6 +11,12 @@
 using namespace std;
 using namespace chrono;
 
+int liczba_porownan = 0, liczba_zamian = 0;
+
+void print_vector(vector<int>& arr);
+vector<int> generate_random_vector(int n, int min, int max);
+
 void merge_sort(vector<int>& arr, int left, int right);
+void bubble_sort(vector<int>& arr);
 
 #endif
